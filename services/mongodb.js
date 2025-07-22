01c8351d5bb6346438b219fb27ba7d377dc9df08
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-07-22 19:16:07 -0400

    fix: server not starting

--- services/mongodb.js
+++ services/mongodb.js
@@ -16,9 +16,6 @@ const connectDB = async () => {
       isDevMode: dbConfig.options.dbName.includes('-dev')
     });
     
-    // 🧠 Disable autoIndex in production to prevent timeout on serverless platforms
-    mongoose.set('autoIndex', process.env.NODE_ENV !== 'production');
-    console.log(`MongoDB autoIndex was set: `);
     const conn = await mongoose.connect(dbConfig.url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -31,7 +28,6 @@ const connectDB = async () => {
       w: "majority"
     });
     
-    // console.log('📬 Received form submission');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log(`Using database: ${conn.connection.name}`);
     console.log(`Current database settings:`, {
@@ -48,6 +44,61 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+
+
+
+// const mongoose = require("mongoose");
+// const dbConfig = require("../config/db.config.js");
+
+// // Handle deprecation warning
+// mongoose.set('strictQuery', true);
+
+// const connectDB = async () => {
+//   try {
+//     if (!dbConfig.url) {
+//       throw new Error('MONGODB_URI is not defined');
+//     }
+    
+//     console.log('Attempting to connect to MongoDB with config:', {
+//       database: dbConfig.database,
+//       dbName: dbConfig.options.dbName,
+//       isDevMode: dbConfig.options.dbName.includes('-dev')
+//     });
+    
+//     // 🧠 Disable autoIndex in production to prevent timeout on serverless platforms
+//     mongoose.set('autoIndex', process.env.NODE_ENV !== 'production');
+//     console.log(`MongoDB autoIndex was set: `);
+//     const conn = await mongoose.connect(dbConfig.url, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       dbName: dbConfig.database,
+//       serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+//       connectTimeoutMS: 30000,
+//       socketTimeoutMS: 45000,
+//       ssl: true,
+//       retryWrites: true,
+//       w: "majority"
+//     });
+    
+//     // console.log('📬 Received form submission');
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     console.log(`Using database: ${conn.connection.name}`);
+//     console.log(`Current database settings:`, {
+//       url: dbConfig.url.split('@')[1], // Log URL without credentials
+//       databaseName: conn.connection.name,
+//       collections: Object.keys(conn.connection.collections)
+//     });
+    
+//     return conn;
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 
 
 
