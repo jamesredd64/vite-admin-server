7270f8997672f970d7fee92dc674f09af4700b99
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-07-22 16:40:52 -0400

    fix: changes to stop index bldg crashing

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const config = {
-  url: process.env.MONGODB_URI || 'mongodb+srv://jredd2013:X9iwELRRwqCCb7kc@mern-cluster.oistpfp.mongodb.net/?retryWrites=true&w=majority',
+  url: process.env.MONGODB_URI || 'mongodb+srv://jredd2013:X9iwELRRwqCCb7kc@mern-cluster.oistpfp.mongodb.net/mongo_users-react-dev?retryWrites=true&w=majority',
   database: 'mongo_users-react-dev',
   options: {
     useNewUrlParser: true,
@@ -13,5 +13,23 @@ const config = {
   }
 };
 
-
 module.exports = config;
+
+
+// const config = {
+//   url: process.env.MONGODB_URI || 'mongodb+srv://jredd2013:X9iwELRRwqCCb7kc@mern-cluster.oistpfp.mongodb.net/?retryWrites=true&w=majority',
+//   database: 'mongo_users-react-dev',
+//   options: {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: 'mongo_users-react-dev',
+//     serverSelectionTimeoutMS: 30000,
+//     connectTimeoutMS: 30000,
+//     socketTimeoutMS: 45000,
+//     retryWrites: true,
+//     w: 'majority'
+//   }
+// };
+
+
+// module.exports = config;
