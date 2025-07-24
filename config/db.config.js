01096f1aa309de4eebe18939a5fed3999a58abd9
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-07-24 09:37:22 -0400

    fix: removed per log useNewUrlParser: true,      useUnifiedTopology: true,

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,8 @@ const config = {
   url: process.env.MONGODB_URI || 'mongodb+srv://jredd2013:X9iwELRRwqCCb7kc@mern-cluster.oistpfp.mongodb.net/mongo_users-react-dev?retryWrites=true&w=majority',
   database: 'mongo_users-react-dev',
   options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
     dbName: 'mongo_users-react-dev',
     serverSelectionTimeoutMS: 30000,
     connectTimeoutMS: 30000,
