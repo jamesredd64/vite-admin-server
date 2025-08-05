6fbe90f5dda902419b84b13b3d8abe064bad48c2
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-08-05 01:04:14 -0400

    changed db name to showcase-collection

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 const config = {
-  url: process.env.MONGODB_URI || 'mongodb+srv://jredd2013:X9iwELRRwqCCb7kc@mern-cluster.oistpfp.mongodb.net/mongo_users-react-dev?retryWrites=true&w=majority',
-  database: 'mongo_users-react-dev',
+  url: process.env.MONGODB_URI || 'mongodb+srv://showcase_admin:wt2TSeIH5dLdHtxw@showcase.vd6up8i.mongodb.net/?retryWrites=true&w=majority&appName=showcase',
+  name: 'showcase-collection',
   options: {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
-    dbName: 'mongo_users-react-dev',
+    dbName: config.name,
     serverSelectionTimeoutMS: 30000,
     connectTimeoutMS: 30000,
     socketTimeoutMS: 45000,
@@ -18,3 +18,21 @@ const config = {
 module.exports = config;
 
 
+// const config = {
+//   url: process.env.MONGODB_URI || 'mongodb+srv://showcase_admin:wt2TSeIH5dLdHtxw@showcase.vd6up8i.mongodb.net/?retryWrites=true&w=majority&appName=showcase',
+//   database: 'showcase-collection',
+//   options: {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//     dbName: 'showcase-collection',
+//     serverSelectionTimeoutMS: 30000,
+//     connectTimeoutMS: 30000,
+//     socketTimeoutMS: 45000,
+//     retryWrites: true,
+//     w: 'majority',
+//     autoIndex: false
+//       // process.env.NODE_ENV !== 'production' // Disable auto-indexing
+//   }
+// };
+
+// module.exports = config;
